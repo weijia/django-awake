d6813be6a62c4e704c77bc5a1801dafc78e42060
John Anderson <johna@rllco.com>
2011-06-03 17:04:07 -0400

    Removed 'messages' from INSTALLED_APPS, to support Django 1.1 out of the box.

--- output/settings.py
+++ output/settings.py
@@ -88,7 +88,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
-    'django.contrib.messages',
+#    'django.contrib.messages',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
